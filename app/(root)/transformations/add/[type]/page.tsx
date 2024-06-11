a361c03efa85f06cc1e30cf33abcd1b83c8f1cdc
faizanhasan <mdfaizanalam367@gmail.com>
2024-06-11 10:16:22 +0530

    Update dependencies and fix UI issues

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,34 +1,33 @@
-// import Header from '@/components/shared/Header';
-// import TransformationForm from '@/components/shared/TransformationForm';
-// import { transformationTypes } from '@/constants'
-// import { getUserById } from '@/lib/actions/user.actions';
-// import { auth } from '@clerk/nextjs/server';
-// import { redirect } from 'next/dist/server/api-utils';
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = async () => {
-  // const { userId } = auth();
-  // const transformation = transformationTypes[type];
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
 
-  // if(!userId) redirect('/sign-in')
+  if(!userId) redirect('/sign-in')
 
-  // const user = await getUserById(userId);
+  const user = await getUserById(userId);
 
   return (
     <>
-    <h1>AddTransformationTypePage</h1>
-      {/* <Header
+      <Header 
         title={transformation.title}
         subtitle={transformation.subTitle}
       />
     
       <section className="mt-10">
         <TransformationForm
-          action="Add"
+        action="Add"
           userId={user._id}
           type={transformation.type as TransformationTypeKey}
           creditBalance={user.creditBalance}
         />
-      </section> */}
+      </section>
     </>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -106,3 +106,50 @@ export const getImageSize = (
   }
   return image?.[dimension] || 1000;
 };
+
+// DOWNLOAD IMAGE
+export const download = (url: string, filename: string) => {
+  if (!url) {
+    throw new Error("Resource URL not provided! You need to provide one");
+  }
+
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = blobURL;
+
+      if (filename && filename.length)
+        a.download = `${filename.replace(" ", "_")}.png`;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch((error) => console.log({ error }));
+};
+
+// DEEP MERGE OBJECTS
+export const deepMergeObjects = (obj1: any, obj2: any) => {
+  if(obj2 === null || obj2 === undefined) {
+    return obj1;
+  }
+
+  let output = { ...obj2 };
+
+  for (let key in obj1) {
+    if (obj1.hasOwnProperty(key)) {
+      if (
+        obj1[key] &&
+        typeof obj1[key] === "object" &&
+        obj2[key] &&
+        typeof obj2[key] === "object"
+      ) {
+        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+      } else {
+        output[key] = obj1[key];
+      }
+    }
+  }
+
+  return output;
+};
